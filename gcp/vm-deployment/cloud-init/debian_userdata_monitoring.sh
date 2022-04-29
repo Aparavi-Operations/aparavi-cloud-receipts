dbb6860bf0f2b388a816fb6d10e2cac77adfe2b8
akuplensky <aleksey.kuplenskiy@aparavi.com>
2022-04-29 19:51:16 +0300

    wip

--- gcp/vm-deployment/cloud-init/debian_userdata_monitoring.sh
+++ gcp/vm-deployment/cloud-init/debian_userdata_monitoring.sh
@@ -55,40 +55,10 @@ cd /root/aparavi-cloud-receipts && git checkout $${MONITORING_BRANCH}
 cp -r /root/aparavi-cloud-receipts/monitoring/templates/monitoring /root/
 rm -f /root/monitoring/vmagent/scrape_azure.yml
 rm -f /root/monitoring/vmagent/scrape_ec2.yml
-#cp /root/aparavi-cloud-receipts/monitoring/templates/monitoring/vmagent/scrape_gcp.yml /root/monitoring/vmagent/scrape_gcp.yml
-sudo cat <<EOF > /root/monitoring/vmagent/scrape_gcp.yml
----
-
-- job_name: 'nodeexporter_gcp'
-
-  static_configs:
-    - labels:
-        service: 'aparavi'
-        component: 'aggregator'
-        subcomponent: 'app'
-        env: '<<deployment>>'
-      targets:
-        - '<<aggregator_ip>>:9100'
-    - labels:
-        service: 'aparavi'
-        component: 'collector'
-        subcomponent: 'app'
-        env: '<<deployment>>'
-      targets:
-         - '<<collector_ip>>:9100'
-    - labels:
-        service: 'monitoring'
-        component: 'monitoring'
-        subcomponent: 'app'
-        env: '<<deployment>>'
-      targets:
-         - '<<monitoring_ip>>:9100'
-EOF
-sed -i 's/<<deployment>/${deployment_name}/g' /root/monitoring/vmagent/scrape_gcp.yml
+sed -i 's/<<deployment>>/${deployment_name}/g' /root/monitoring/vmagent/scrape_gcp.yml
 sed -i 's/<<aggregator_ip>>/${aggregator_private_ip}/g' /root/monitoring/vmagent/scrape_gcp.yml
 sed -i 's/<<collector_ip>>/${collector_private_ip}/g' /root/monitoring/vmagent/scrape_gcp.yml
 sed -i 's/<<monitoring_ip>>/${monitoring_private_ip}/g' /root/monitoring/vmagent/scrape_gcp.yml
-rm /root/monitoring/vmagent/scrape_gcp.yml
 cp /root/monitoring/aparavi-monitoring.service /etc/systemd/system/aparavi-monitoring.service
 systemctl daemon-reload
 systemctl enable aparavi-monitoring
