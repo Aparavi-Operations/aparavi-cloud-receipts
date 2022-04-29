2cd0554692578511f6d9215d4fe318a4b5809e04
roman-dimitrenko <101823732+roman-dimitrenko@users.noreply.github.com>
2022-04-29 21:09:57 +0300

    Update variables.tf

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,5 +1,5 @@
 variable "KEY_NAME" {
-  description = "MANDATORY SSH Key name to attach to ec2 instances"
+  description = "MANDATORY - SSH Key name to attach to ec2 instances"
   default     = ""
   
   validation {
@@ -9,37 +9,37 @@ variable "KEY_NAME" {
 }
 
 variable "MANAGEMENT_NETWORK" {
-  description = "MANDATORY Management network to allow connections from (e.g. 0.0.0.0/0 for all incoming connection)"
+  description = "MANDATORY - Allow connections from Management network (e.g. 0.0.0.0/0 for all incoming connection from the Internet)"
   default     = ""
   
   validation {
     condition     = length(var.MANAGEMENT_NETWORK) > 0
-    error_message = "You must specify the network cidr for security group ingress rule."
+    error_message = "You must specify the network CIDR for security group ingress rule."
   }
 }
 
 variable "PLATFORM" {
-  description = "MANDATORY Platform to attach aggregator to (e.g. preview.aparavi.com)"
+  description = "MANDATORY - Platform to attach an aggregator to (e.g. preview.aparavi.com)"
   default     = ""
 
   validation {
     condition     = length(var.PLATFORM) > 0
-    error_message = "You must specify the host for platform so aggregator could connect to it. (e.g. preview.aparavi.com)"
+    error_message = "You must specify the host for the platform so aggregator could connect to it. (e.g. preview.aparavi.com)."
   }
 }
 
 variable "PARENT_ID" {
-  description = "ID from Aparavi Portal"
+  description = "MANDATORY - activeNodeId from Aparavi Portal"
   default     = ""
 
   validation {
     condition     = length(var.PARENT_ID) > 0
-    error_message = "You must specify the parent_id from th platform in order to place there the new aggregator instance."
+    error_message = "You must specify the parent_id from the platform in order to attach new aggregator instance."
   }
 }
 
 variable "DEPLOYMENT" {
-  description = "MANDATORY Unique name of the deployment. Multiple deployments of this stack in single VPC are supported (e.g. attempt1)"
+  description = "MANDATORY - Unique name of the deployment. Multiple deployments of this stack in single VPC are supported (e.g. attempt1)"
   default     = ""
   
   validation {
