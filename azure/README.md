af83e5eedd0a4964a0aaba21979109c2e6447562
bagratte <bagratte@live.com>
2022-05-25 18:21:32 +0400

    Add Azure AKS

---
+++ azure/README.md
@@ -0,0 +1,15 @@
+# Aparavi on Azure
+
+## Requirements
+
+Make sure you have installed
+[Azure CLI](https://docs.microsoft.com/en-us/cli/azure/).
+Installation instructions are available at
+https://docs.microsoft.com/en-us/cli/azure/install-azure-cli
+You should also make sure you are signed in sith Azure CLI. Any method described
+at https://docs.microsoft.com/en-us/cli/azure/authenticate-azure-cli should do.
+
+## Organization of Azure cloud receipts
+
+[`vm/`](vm/): Deployment example of Aparavi on Azure Virtual Machines
+[`aks`](aks/): Deployment example of Aparavi on Azure Kubernetes Service (AKS)
---
+++ azure/aks/variables.tf
@@ -0,0 +1,62 @@
+variable "location" {
+  description = "Azure location where resources reside"
+  type        = string
+  default     = "eastus"
+}
+
+variable "name" {
+  description = <<-EOT
+    Main name of resources, such as network, GKE cluster, Cloud SQl...
+  EOT
+  type        = string
+  default     = "aparavi"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = { service : "aparavi" }
+}
+
+variable "aks_agents_size" {
+  description = "Virtual machine size for the Kubernetes agents"
+  type        = string
+  default     = "Standard_B4ms"
+}
+
+variable "mysql_sku_name" {
+  description = <<-EOT
+    The SKU Name for MySQL Server. The name of the SKU follows the
+    tier + family + cores pattern (e.g. B_Gen4_1, GP_Gen5_8)
+  EOT
+  type        = string
+  default     = "GP_Gen5_2"
+}
+
+variable "platform_host" {
+  description = "Aparavi platform host to connect aggregator to"
+  type        = string
+}
+
+variable "platform_node_id" {
+  description = "Aparavi platform node ID to connect aggregator to"
+  type        = string
+}
+
+variable "aggregator_node_name" {
+  description = "Aggregator node name. Default: \"$${var.name}-aggregator\""
+  type        = string
+  default     = ""
+}
+
+variable "collector_node_name" {
+  description = "Collector node name. Default: \"$${var.name}-collector\""
+  type        = string
+  default     = ""
+}
+
+variable "generate_sample_data" {
+  description = "Generate sample data for collector"
+  type        = bool
+  default     = false
+}
