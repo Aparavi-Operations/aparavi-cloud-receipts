3d2c6c6e48fdd90ee1d841ce51f94bdafecf6744
Ilya Taskaev <ansustiwaz@gmail.com>
2022-05-29 17:30:59 +0300

    fix

--- aparavi-connect/azure/vm/variables.tf
+++ aparavi-connect/azure/vm/variables.tf
@@ -21,13 +21,12 @@ variable "platform" {
 }
 
 variable "parent_id" {
-  type    = string
-  default = "bbbbbbbb-bbbb-bbbb-bbbb-brdimitrenko"
+  type = string
 }
 
 variable "bastion_size" {
   type    = string
-  default = "Standard_D2s_v4"
+  default = "Standard_DS1"
 }
 
 variable "db_shape" {
@@ -36,8 +35,7 @@ variable "db_shape" {
 }
 
 variable "collector_storage_size" {
-  type    = number
-  default = 200
+  type = number
 }
 
 variable "db_user" {
@@ -46,23 +44,22 @@ variable "db_user" {
 }
 
 variable "db_password" {
-  type    = string
-  default = "supers3CretP@ssw0rd"
+  type = string
 }
 
-variable "node_size" {
+variable "aggregator_size" {
   type    = string
-  default = "Standard_D2s_v4"
+  default = "Standard_E4bs_v5"
 }
 
 variable "collector_size" {
   type    = string
-  default = "Standard_D2s_v4"
+  default = "Standard_E4bs_v5"
 }
 
 variable "monitoring_size" {
   type    = string
-  default = "Standard_D2s_v4"
+  default = "Standard_DS1"
 }
 
 variable "monitoring_role_name" {
@@ -71,8 +68,7 @@ variable "monitoring_role_name" {
 }
 
 variable "ssh_key" {
-  type    = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDqyefZ//CftxHIl+jMuwTSoYRmpEF73JkVXXNp4RKvgDJjgsv6a2mz//WgIUe9YSzw/9VgJPr5XxVJIZgOdEz8nscl2aT8wjy6cM/EGKQfytYSSOjf9xAE6+/OJ8cDum2NwXo9PQ4POHlOUC8ZZxVtKFzvhQHLdDUromaPLd8GyFeq5a23yiT30J/FoOsFtgPgJBMQL/0c9ZcsLjdc9yUw9qXPv76ztPNx70V1ecTQd144LP+0/IoT8LhCzNz1w2iN3BmIzc9v4ga6Jn01borN8FjCMEtiuuMYB19NYPboc+lg49BQaKSQfRM8hOGpUv6vAQPke8lr4xrmy3Fof67r user@Ilya"
+  type = string
 }
 
 variable "tags" {
@@ -80,3 +76,4 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
