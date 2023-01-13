641425b560913bc0e2bcd083251cb839e16ab5d5
Ilya Taskaev <Ilya.Taskaev@aparavi.com>
2023-01-13 13:59:45 +0100

    provider configured

--- aparavi-connect/azure/vm/provider.tf
+++ aparavi-connect/azure/vm/provider.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = ">=3.0.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
